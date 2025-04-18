51bd65f335e011e1da568fb62bffbd2278895e27
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2025-04-18 17:54:54 +0530

    fixeddddd

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID, // Google client ID from environment variable
             clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Google client secret from environment variable
-            callbackURL: "http://buzztube-production.up.railway.app//api/auth/google/callback", // Callback URL after Google authentication
+            callbackURL: "https://buzztube-production.up.railway.app//api/auth/google/callback", // Callback URL after Google authentication
         },
         async (accessToken, refreshToken, profile, cb) => {
             try {
