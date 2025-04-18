c898fae29b2970439ded38a96405d975ba04ff8c
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2025-04-18 17:15:13 +0530

    made responsive and fixed some things

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID, // Google client ID from environment variable
             clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Google client secret from environment variable
-            callbackURL: "https://buzztube-production.up.railway.app/api/auth/google/callback", // Callback URL after Google authentication
+            callbackURL: "http://localhost:3000/api/auth/google/callback", // Callback URL after Google authentication
         },
         async (accessToken, refreshToken, profile, cb) => {
             try {
